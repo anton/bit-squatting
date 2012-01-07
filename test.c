1c7e77169e369c97b81254f37ef54c633c57fcf8
Anton <info@antonhaglund.se>
2012-01-07 18:24:28 +0100

    Updating test case

--- test.c
+++ test.c
@@ -3,6 +3,8 @@
 
 int main()
 {
-    assert(7 == flip_bit(0b00000101, 1));
+    assert(7 == flip_bit(0b0101, 1));
+    assert(0b0100 == flip_bit(0b0101, 0));
+    assert(0b0111 == flip_bit(0b1111, 3));
     return 0;
 }
