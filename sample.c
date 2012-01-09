1ab6d2484e0441ad59fdbcb988068118615482d2
Anton <info@antonhaglund.se>
2012-01-09 21:20:15 +0100

    code for profiling

--- sample.c
+++ sample.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/time.h>
+
+#define INITPROF struct timeval before, after;
+#define STARTPROF gettimeofday(&before, NULL);
+#define STOPPROF gettimeofday(&after, NULL);
+#define REPORTPROF(a) printf("%06ld usec - %s\n", after.tv_usec - before.tv_usec, (a));
 
 int main(int argc, char *argv[])
 {
@@ -26,10 +32,24 @@ int main(int argc, char *argv[])
         exit(-1);
     }
 
+    INITPROF;
+
+    STARTPROF;
     url = create_url(argv[1]);
+    STOPPROF;
+    REPORTPROF("create_url");
+
+    STARTPROF;
     bs_entries = generate_entries(url, ntimes);
+    STOPPROF;
+    REPORTPROF("generate_entries");
+
+    STARTPROF;
     bitsquat_entries(bs_entries);
-    print_bs_entries(bs_entries);
+    STOPPROF;
+    REPORTPROF("bitsquat_entries");
+
+    /* print_bs_entries(bs_entries); */
 
     return 0;
 }
