37b63037fc766a040ba5f5e31d289d0ebce2c302
anton <haglundanton+github@gmail.com>
2025-11-28 14:54:26 +0100

    Add LICENSE

--- bs.h
+++ bs.h
@@ -1,3 +1,27 @@
+/**
+ * LICENSE
+ *
+ * Use this software at your own risk. The author(s) and contributors provide the
+ * code "as is", without warranty of any kind, express or implied, including but
+ * not limited to warranties of merchantability, fitness for a particular purpose,
+ * or non-infringement. The author(s) and contributors are not liable for any
+ * direct, indirect, incidental, special, exemplary, or consequential damages
+ * (including, without limitation, procurement of substitute goods or services;
+ * loss of use, data, or profits; or business interruption) arising out of the use
+ * or inability to use the software, even if advised of the possibility of such
+ * damages.
+ *
+ * This project performs operations on input strings and allocates memory;
+ * improper use, malformed input, or modification of the code may cause crashes,
+ * memory corruption, data loss, or security vulnerabilities. You are responsible
+ * for reviewing, testing, and securing the code before using it in production or
+ * on sensitive systems.
+ *
+ * If you redistribute or modify the software, you must do so under the terms of
+ * its license and retain this disclaimer in accompanying documentation.
+ *
+ **/
+
 #include <string.h>
 
 struct Url {
